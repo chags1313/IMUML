46757f75940894f470ec791adecfb1426a0a2c23
Cole Hagen <61998370+chags1313@users.noreply.github.com>
2023-06-29 11:28:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,24 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-st.title('IMU ML')
+st.set_page_config(
+    page_title="🤾‍♀️ IMU ML",
+    page_icon="🤾‍♀️",
+    layout="wide"
+)
+
+st.title('🤾‍♀️ IMU ML')
+st.markdown("Predicting activities from movement data and machine learning")
+
+upload, annotation, ml, results = st.tabs(['Upload', 'Annotation', 'ML', 'Results')
+
+with upload:
+  file = st.file_uploader("Upload your Phyphox Data File")
+
+  if file is not None:
+    with upload:
+      data = pd.read_csv(file)
+      x = st.selectbox('X Axis', data.columns)
+      y = st.selectbox('Y Axis', data.columns)
+      fig = px.line(data, x = x, y = y)
+      st.plotly_chart(fig)
